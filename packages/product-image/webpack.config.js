38aefb0b4df65d847c3835c3f4f3e42965090c45
candido-luiz <candidoluiz097@gmail.com>
2022-07-07 17:37:40 -0300

    Subindo copia do repo para github

---
+++ packages/product-image/webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  entry: './src/index',
+  cache: false,
+
+  mode: 'development',
+  devtool: 'source-map',
+
+  optimization: {
+    minimize: false
+  },
+
+  output: {
+    publicPath: 'http://localhost:3002/'
+  },
+
+  resolve: {
+    extensions: ['.jsx', '.js', '.json']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [require.resolve('@babel/preset-react')]
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'productImage',
+      // library: { type: 'var', name: 'productImage' },
+      filename: 'remoteEntry.js',
+      remotes: {
+        home: 'home@http://localhost:3001/remoteEntry.js',
+        store: 'store@http://localhost:3005/remoteEntry.js',
+      },
+      exposes: {
+        "./ProductImage": './src/index'
+      },
+      shared: ['single-spa-html']
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      chunks: ['main']
+    })
+  ]
+};
